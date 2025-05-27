8c7c6e276484bc463c16e60e0da6ef26acfd0c47
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2025-05-28 07:14:15 +0900

    secound commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('todos.index', compact('todos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('todos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ]);
+
+        Todo::create($request->all());
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        return view('todos.edit', compact('todo'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+       $request->validate([
+        'title' => 'required',
+       ]);
+
+       $todo = Todo::findOrFail($is);
+       $todo->update($request->all());
+       return redirect()->route('todos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        return redirect()->route('todos.index');
+    }
+}
